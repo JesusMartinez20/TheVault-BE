3a98b9e683ee5aa0b5c6ff5ab95b9c8dde8e89ec
jessusu20 <c.f.cubers@gmail.com>
2021-06-15 16:47:06 -0500

    BAase de datos v2

--- thevault.sql
+++ thevault.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 07-06-2021 a las 21:14:27
+-- Tiempo de generación: 15-06-2021 a las 23:43:18
 -- Versión del servidor: 10.4.8-MariaDB
 -- Versión de PHP: 7.3.11
 
@@ -38,6 +38,13 @@ CREATE TABLE `comentarios` (
   `id_pelicula` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Volcado de datos para la tabla `comentarios`
+--
+
+INSERT INTO `comentarios` (`id`, `titulo`, `opinion`, `calificacion`, `fecha`, `usuario`, `id_pelicula`) VALUES
+(1, 'Que wena pelicula', 'Ufff, es mi pelicula favorita', 5, '2021-06-14', 'Jessusu20', 1);
+
 -- --------------------------------------------------------
 
 --
@@ -51,6 +58,15 @@ CREATE TABLE `participacion` (
   `id_staff` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Volcado de datos para la tabla `participacion`
+--
+
+INSERT INTO `participacion` (`id`, `rol`, `id_pelicula`, `id_staff`) VALUES
+(1, 'Director', 1, 1),
+(2, 'Actor', 1, 2),
+(3, 'Actor', 1, 3);
+
 -- --------------------------------------------------------
 
 --
@@ -63,8 +79,17 @@ CREATE TABLE `pelicula` (
   `duracion` smallint(6) DEFAULT NULL,
   `pais` varchar(50) DEFAULT NULL,
   `imagen` varchar(300) DEFAULT NULL,
-  `fecha_estreno` date DEFAULT NULL
+  `fecha_estreno` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `pelicula`
+--
+
+INSERT INTO `pelicula` (`id`, `nombre`, `duracion`, `pais`, `imagen`, `fecha_estreno`) VALUES
+(1, 'Pulp Fiction', 178, 'Estados Unidos', 'https://cdn.shopify.com/s/files/1/0265/2769/4934/products/pulp_fiction_ver2_xxlg_530x@2x.jpg?v=1579934456', 1994),
+(2, 'Taxi Driver', 114, 'Estados Unidos', 'https://images-na.ssl-images-amazon.com/images/I/51TBx9tzHFL._AC_.jpg', 1976),
+(3, 'The Godfather', 178, 'Estados Unidos', 'https://movieposters2.com/images/1535670-b.jpg', 1972);
 
 -- --------------------------------------------------------
 
@@ -81,6 +106,13 @@ CREATE TABLE `premio` (
   `id_pelicula` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Volcado de datos para la tabla `premio`
+--
+
+INSERT INTO `premio` (`id`, `categoria`, `fecha`, `lugar`, `academia`, `id_pelicula`) VALUES
+(1, 'Mejor guion original', '0000-00-00', 'Angeles, California', 'Premios Oscar', 1);
+
 -- --------------------------------------------------------
 
 --
@@ -94,6 +126,15 @@ CREATE TABLE `staff` (
   `fecha_nacimiento` date DEFAULT NULL,
   `sexo` char(2) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `staff`
+--
+
+INSERT INTO `staff` (`id`, `nombre`, `nacinalidad`, `fecha_nacimiento`, `sexo`) VALUES
+(1, 'Quentin Tarantino', 'Estados Unidos', '0000-00-00', 'M'),
+(2, 'Samuel L. Jackson', 'Estados Unidos', '0000-00-00', 'M'),
+(3, 'John Travolta', 'Estados Unidos', '0000-00-00', 'M');
 
 -- --------------------------------------------------------
 
@@ -112,6 +153,13 @@ CREATE TABLE `usuario` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`username`, `fecha_registro`, `nombre`, `contrasena`, `fecha_nacimiento`, `correo`, `avatar`) VALUES
+('Jessusu20', '2021-06-14', 'Jesus Martinez', '1234', '0000-00-00', 'jesusumarcor20@gmail.com', 1);
+
+--
 -- Índices para tablas volcadas
 --
 
@@ -120,16 +168,16 @@ CREATE TABLE `usuario` (
 --
 ALTER TABLE `comentarios`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `usuario` (`usuario`),
-  ADD KEY `id_pelicula` (`id_pelicula`);
+  ADD KEY `comentarios_ibfk_1` (`usuario`),
+  ADD KEY `comentarios_ibfk_2` (`id_pelicula`);
 
 --
 -- Indices de la tabla `participacion`
 --
 ALTER TABLE `participacion`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `id_pelicula` (`id_pelicula`),
-  ADD KEY `id_staff` (`id_staff`);
+  ADD KEY `participacion_ibfk_1` (`id_pelicula`),
+  ADD KEY `participacion_ibfk_2` (`id_staff`);
 
 --
 -- Indices de la tabla `pelicula`
@@ -142,7 +190,7 @@ ALTER TABLE `pelicula`
 --
 ALTER TABLE `premio`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `id_pelicula` (`id_pelicula`);
+  ADD KEY `premio_ibfk_1` (`id_pelicula`);
 
 --
 -- Indices de la tabla `staff`
@@ -157,6 +205,40 @@ ALTER TABLE `usuario`
   ADD PRIMARY KEY (`username`);
 
 --
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `comentarios`
+--
+ALTER TABLE `comentarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `participacion`
+--
+ALTER TABLE `participacion`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `pelicula`
+--
+ALTER TABLE `pelicula`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `premio`
+--
+ALTER TABLE `premio`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `staff`
+--
+ALTER TABLE `staff`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- Restricciones para tablas volcadas
 --
 
@@ -164,8 +246,8 @@ ALTER TABLE `usuario`
 -- Filtros para la tabla `comentarios`
 --
 ALTER TABLE `comentarios`
-  ADD CONSTRAINT `comentarios_ibfk_1` FOREIGN KEY (`usuario`) REFERENCES `usuario` (`username`),
-  ADD CONSTRAINT `comentarios_ibfk_2` FOREIGN KEY (`id_pelicula`) REFERENCES `pelicula` (`id`);
+  ADD CONSTRAINT `comentarios_ibfk_1` FOREIGN KEY (`usuario`) REFERENCES `usuario` (`username`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `comentarios_ibfk_2` FOREIGN KEY (`id_pelicula`) REFERENCES `pelicula` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Filtros para la tabla `participacion`
