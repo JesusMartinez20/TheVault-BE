da962128b22639457b1047a73802c02910daef01
luisfgarav <luisfgaravil@gmail.com>
2021-06-17 02:46:57 -0500

    Actualización de comentarios en la base de datos

--- thevault.sql
+++ thevault.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.1
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 17-06-2021 a las 07:34:02
--- Versión del servidor: 10.4.8-MariaDB
--- Versión de PHP: 7.3.11
+-- Tiempo de generación: 17-06-2021 a las 09:46:05
+-- Versión del servidor: 10.4.14-MariaDB
+-- Versión de PHP: 7.4.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -43,8 +42,37 @@ CREATE TABLE `comentarios` (
 --
 
 INSERT INTO `comentarios` (`id`, `titulo`, `opinion`, `calificacion`, `fecha`, `usuario`, `id_pelicula`) VALUES
-(1, 'Que wena pelicula', 'Ufff, es mi pelicula favorita', 5, '2021-06-14', 'Jessusu20', 1),
-(2, 'Me encanto', 'Me gusto mucho la musica y el final!', 4, '2021-06-17', 'Jessusu20', 2);
+(1, 'Que buena película', 'Ufff, es mi pelicula favorita', 5, '2021-06-14', 'Jessusu20', 1),
+(2, 'Me encanto', 'Me gusto mucho la musica y el final!', 4, '2021-06-17', 'Jessusu20', 2),
+(3, 'Emocionante', 'Vale cada minuto de la película ', 5, '2021-06-17', 'Luisf', 2),
+(4, 'De las mejores', 'No puedo esperar para volver a verla', 4, '2021-06-17', 'Luisf', 7),
+(5, 'Atrapante', 'Cada escena es una obra maestra', 4, '2021-06-17', 'Luisf', 4),
+(6, 'Me encantó', 'Ha sido una montaña de emociones, pero el ritmo me parece lento', 4, '2021-06-17', 'LuisF', 9),
+(7, 'Impresionante', 'Es de las mejores películas que he visto, me encanta la trama', 5, '2021-06-17', 'LuisF', 1),
+(8, 'Increíble ', 'Definitivamente el manejo de cámara, el guión y la música te atrapan', 4, '2021-06-17', 'Luisf', 10),
+(9, 'Lo mejor de Iñarritu', 'Uno de las mejores obras hechas en el cine mexicano ', 5, '2021-06-17', 'Jessusu20', 10),
+(10, 'Me sorprendio', 'No me esperaba ver tanta maestría en esta película, me voló la cabeza', 4, '2021-06-17', 'Jessusu20', 5),
+(11, 'El inicio de mejor saga', 'Simplemente te deja sin palabras, una historia atrapante', 5, '2021-06-17', 'Jessusu20', 3),
+(12, 'Gran película ', 'Muchas veces subestimada, esta película es grandiosa pero con fallos de ritmo', 3, '2021-06-17', 'TheRaven0', 6),
+(13, 'Recomendada', 'Una gran película del género que no cae en la mediocridad a pesar de la sobredemanda', 4, '2021-06-17', 'TheRaven0', 8),
+(14, 'Un clásico imperdible', 'Ideal para introducirse al cine de Tarantino', 5, '2021-06-17', 'TheRaven0', 1),
+(15, 'Obra maestra', 'No sólo marcó el cine de una generación, marcó precedentes de todas las áreas del cine ', 4, '2021-06-17', 'TheRaven0', 2),
+(16, 'Magnífica ', 'Es una película que no te deja respirar de principio a fin, me encantan sus secuencias', 5, '2021-06-17', 'TheRaven0', 4),
+(17, 'Buena película ', 'Una forma refrescante del cine de época. ', 4, '2021-06-17', 'TheRaven0', 9),
+(18, 'Me gustó ', 'La historia es propósito a y atrapan te, los juegos de cámara son sorprendentes', 4, '2021-06-17', 'VioletUmi', 5),
+(19, 'Buena', 'Me parece que una saga muy buena pero no perfecta', 3, '2021-06-17', 'VioletUmi', 3),
+(20, 'Recomendada', 'Si tienen la oportunidad de verla en una sala de cine, ¡se los recomiendo! ', 4, '2021-06-17', 'VioletUmi', 2),
+(21, 'Reflexiva', 'Esta película marcó un precedente para el cine mexicano. Definitivamente muy buena. ', 5, '2021-06-17', 'VioletUmi', 10),
+(22, 'Sorpresiva', 'Disfruté mucho de esta película, el sonido y las escenas de esta película valen la pena en una pantalla grande. ', 5, '2021-06-17', 'VioletUmi', 4),
+(23, 'Intrigante', 'Me gustó la manera en que esta película te atrapa desde el principio. ', 4, '2021-06-17', 'VioletUmi', 6),
+(24, 'Tienen que verla', 'Si no la han visto, denle una oportunidad. No se vana a arrepentir. ', 4, '2021-06-17', 'Jessusu20', 6),
+(25, 'Simplemente espectacular', 'Cada plano te deja con el aliento helado. Pudo tener cosas mejores pero en su género se lleva el podio. ', 4, '2021-06-17', 'Jessusu20', 9),
+(26, 'Espectacular ', 'Tienen que verla', 4, '2021-06-17', 'Chit0mx', 1),
+(27, 'La recomiendo', 'Una historia que te deja reflexionando, es buenísima. ', 5, '2021-06-17', 'Chit0mx', 4),
+(28, 'Buena película de guerra', 'Es divertida lo suficiente, pero tiene partes poco interesantes. ', 3, '2021-06-17', 'Chit0mx', 9),
+(29, 'Mi favorita ', 'Desde el primer momento se volvió una de mis favoritas ', 5, '2021-06-17', 'Chit0mx', 7),
+(30, 'Increíble ', 'Tiene uno de los mejores intros de la historia del cine. No necesito decir más. ', 5, '2021-06-17', 'TheRaven0', 7),
+(31, 'Gran trama', 'Te deja al borde del asiento en toda la película. ', 5, '2021-06-17', 'Luisf', 8);
 
 -- --------------------------------------------------------
 
@@ -93,7 +121,15 @@ INSERT INTO `participacion` (`id`, `rol`, `id_pelicula`, `id_staff`) VALUES
 (27, 'Actor', 10, 27),
 (28, 'Actor', 10, 28),
 (29, 'Actor', 5, 11),
-(30, 'Actor', 8, 4);
+(30, 'Actor', 8, 4),
+(31, 'Director', 3, 29),
+(32, 'Director', 4, 30),
+(33, 'Director', 5, 31),
+(34, 'Director', 6, 32),
+(35, 'Director', 7, 33),
+(36, 'Director', 8, 32),
+(37, 'Director', 9, 1),
+(38, 'Director', 10, 34);
 
 -- --------------------------------------------------------
 
@@ -124,7 +160,7 @@ INSERT INTO `pelicula` (`id`, `nombre`, `duracion`, `pais`, `imagen`, `fecha_est
 (7, 'Trainspotting', 95, 'Reino Unido', 'https://i.pinimg.com/originals/4a/77/3b/4a773b665f2cd746de7f08fc0cb3b79f.jpg', 1996),
 (8, 'Goodfellas', 148, 'Estados Unidos', 'https://images-na.ssl-images-amazon.com/images/I/51rOnIjLqzL._AC_SY450_.jpg', 1990),
 (9, 'Inglourious Basterds', 153, 'Estados Unidos', 'https://m.media-amazon.com/images/I/51Ro7n9T2fL._AC_.jpg', 2009),
-(10, 'Amores Perros', 155, 'M?xico', 'https://m.media-amazon.com/images/M/MV5BMjQxMWJhMzMtMzllZi00NzMwLTllYjktNTcwZmU4ZmU3NTA0XkEyXkFqcGdeQXVyMTAzMDM4MjM0._V1_.jpg', 2000);
+(10, 'Amores Perros', 155, 'México', 'https://m.media-amazon.com/images/M/MV5BMjQxMWJhMzMtMzllZi00NzMwLTllYjktNTcwZmU4ZmU3NTA0XkEyXkFqcGdeQXVyMTAzMDM4MjM0._V1_.jpg', 2000);
 
 -- --------------------------------------------------------
 
@@ -173,12 +209,12 @@ INSERT INTO `premio` (`id`, `categoria`, `fecha`, `lugar`, `academia`, `id_pelic
 (25, 'Mejor director', '0000-00-00', '', 'Premios BAFTA', 8),
 (26, 'Mejor guion adaptado', '0000-00-00', '', 'Premios BAFTA', 8),
 (27, 'Mejor montaje', '0000-00-00', '', 'Premios BAFTA', 8),
-(28, 'Mejor dise?o de vestuario', '0000-00-00', '', 'Premios BAFTA', 8),
+(28, 'Mejor diseño de vestuario', '0000-00-00', '', 'Premios BAFTA', 8),
 (29, 'Mejor actor de reparto', '0000-00-00', '', 'Premios Oscar', 9),
 (30, 'Mejor actor de reparto', '0000-00-00', '', 'Globos de Oro', 9),
 (31, 'Mejor actor de reparto', '0000-00-00', '', 'Premios BAFTA', 9),
 (32, 'Mejor pelicula en lengua no inglesa', '0000-00-00', '', 'Premios BAFTA', 10),
-(33, 'Premio de la cr?tica', '0000-00-00', '', 'Festival de Cine de Cannes', 10),
+(33, 'Premio de la crítica', '0000-00-00', '', 'Festival de Cine de Cannes', 10),
 (34, 'Mejor pelicula', '0000-00-00', '', 'Festival Internacional de Cine de Tokio', 10);
 
 -- --------------------------------------------------------
@@ -201,10 +237,10 @@ CREATE TABLE `staff` (
 --
 
 INSERT INTO `staff` (`id`, `nombre`, `nacinalidad`, `fecha_nacimiento`, `sexo`, `imagen`) VALUES
-(1, 'Quentin Tarantino', 'Estados Unidos', '0000-00-00', 'M', NULL),
-(2, 'Samuel L. Jackson', 'Estados Unidos', '0000-00-00', 'M', 'https://upload.wikimedia.org/wikipedia/commons/a/a9/Samuel_L._Jackson_2019_by_Glenn_Francis.jpg'),
-(3, 'John Travolta', 'Estados Unidos', '0000-00-00', 'M', 'https://upload.wikimedia.org/wikipedia/commons/4/4f/John_Travolta_Cannes_2018.jpg'),
-(4, 'Robert De Niro', 'Estados Unidos', '0000-00-00', 'M', 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Robert_De_Niro_Cannes_2016.jpg/220px-Robert_De_Niro_Cannes_2016.jpg'),
+(1, 'Quentin Tarantino', 'Estados Unidos', '1963-03-27', 'M', NULL),
+(2, 'Samuel L. Jackson', 'Estados Unidos', '1948-12-21', 'M', 'https://upload.wikimedia.org/wikipedia/commons/a/a9/Samuel_L._Jackson_2019_by_Glenn_Francis.jpg'),
+(3, 'John Travolta', 'Estados Unidos', '1954-02-18', 'M', 'https://upload.wikimedia.org/wikipedia/commons/4/4f/John_Travolta_Cannes_2018.jpg'),
+(4, 'Robert De Niro', 'Estados Unidos', '1943-08-17', 'M', 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Robert_De_Niro_Cannes_2016.jpg/220px-Robert_De_Niro_Cannes_2016.jpg'),
 (5, 'Martin Scorsese', 'Estados Unidos', '1942-11-17', 'M', 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8a/Martin_Scorsese_Berlinale_2010_%28cropped%29.jpg/220px-Martin_Scorsese_Berlinale_2010_%28cropped%29.jpg'),
 (6, 'Jodie Foster', 'Estados Unidos', '1962-11-19', 'F', 'https://upload.wikimedia.org/wikipedia/commons/3/3f/Jodie_Foster_C%C3%A9sars_2011_2_%28cropped%29.jpg'),
 (7, 'Al Pacino', 'Estados Unidos', '1940-04-25', 'M', 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/Al_Pacino.jpg/220px-Al_Pacino.jpg'),
@@ -225,10 +261,16 @@ INSERT INTO `staff` (`id`, `nombre`, `nacinalidad`, `fecha_nacimiento`, `sexo`, 
 (22, 'Ray Liotta', 'Estados Unidos', '1954-12-18', 'M', 'https://i.pinimg.com/originals/bf/4c/2c/bf4c2c0b2bca71466414453e7e0b4f94.jpg'),
 (23, 'Brad Pitt', 'Estados Unidos', '1963-12-18', 'M', 'https://media.fashionnetwork.com/cdn-cgi/image/fit=contain,width=1000,height=1000/m/0d2f/313d/73c9/143a/6875/d46e/d976/bb81/2b1d/b017/b017.jpg'),
 (24, 'Christoph Waltz', 'Austria', '1956-08-04', 'M', 'https://static.wikia.nocookie.net/doblaje/images/2/21/Christoph_waltz_-2019.jpg/revision/latest/top-crop/width/360/height/450?cb=20190816041900&path-prefix=es'),
-(25, 'M?lanie Laurent', 'Francia', '1983-02-21', 'F', 'https://upload.wikimedia.org/wikipedia/commons/5/5d/M%C3%A9lanie_Laurent_C%C3%A9sar_2016_%28cropped%29.jpg'),
-(26, 'Gael Garc?a Bernal', 'M?xico', '1978-11-30', 'M', 'https://upload.wikimedia.org/wikipedia/commons/7/7c/Gael_Garcia_Bernal_2015.jpg'),
-(27, 'Vanessa Bauche', 'M?xico', '1973-02-18', 'F', 'https://upload.wikimedia.org/wikipedia/commons/b/be/MX_MM_CEREMONIA_PEREMIOS_ARIEL_-_35492337790_%28cropped%29.jpg'),
-(28, 'Emilio Echevarr?a', 'M?xico', '1944-07-03', 'M', 'https://images.mubicdn.net/images/cast_member/2357/cache-133949-1613764508/image-w856.jpg?size=800x');
+(25, 'Mélanie Laurent', 'Francia', '1983-02-21', 'F', 'https://upload.wikimedia.org/wikipedia/commons/5/5d/M%C3%A9lanie_Laurent_C%C3%A9sar_2016_%28cropped%29.jpg'),
+(26, 'Gael García Bernal', 'México', '1978-11-30', 'M', 'https://upload.wikimedia.org/wikipedia/commons/7/7c/Gael_Garcia_Bernal_2015.jpg'),
+(27, 'Vanessa Bauche', 'México', '1973-02-18', 'F', 'https://upload.wikimedia.org/wikipedia/commons/b/be/MX_MM_CEREMONIA_PEREMIOS_ARIEL_-_35492337790_%28cropped%29.jpg'),
+(28, 'Emilio Echevarría', 'México', '1944-07-03', 'M', 'https://images.mubicdn.net/images/cast_member/2357/cache-133949-1613764508/image-w856.jpg?size=800x'),
+(29, 'Francis F. Coppola', 'Estados Unidos', '1939-04-07', 'M', 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Francis_Ford_Coppola_2011_CC.jpg/250px-Francis_Ford_Coppola_2011_CC.jpg'),
+(30, 'Alfonso Cuarón', 'México', '1961-11-28', 'M', 'https://upload.wikimedia.org/wikipedia/commons/4/4a/Alfonso_Cuar%C3%B3n%2C_President_jury_Venezia_72_%2825805089406%29_%28cropped%29.jpg'),
+(31, 'Paul T. Anderson', 'Estados Unidos', '1970-06-26', 'M', 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSc_m6f3ce9f7qKtN3A-y7Ks4X56J4cfoQO0R6cUq24r7oVDHpS'),
+(32, 'Martin Scorsese', 'Estados Unidos', '1942-11-17', 'M', 'https://i.pinimg.com/originals/19/79/38/197938b88254d1bda43c8117bc9a4e96.jpg'),
+(33, 'Danny Boyle', 'Inglaterra', '1956-10-20', 'M', 'https://upload.wikimedia.org/wikipedia/commons/f/f0/Danny_Boyle_May_2019.jpg'),
+(34, 'Alejandro Iñárritu', 'México', '1963-08-15', 'M', 'https://wrmx00.epimg.net/radio/imagenes/2020/05/14/de_pelicula_w/1589477720_331445_1589485512_noticia_normal.jpg');
 
 -- --------------------------------------------------------
 
@@ -252,10 +294,10 @@ CREATE TABLE `usuario` (
 
 INSERT INTO `usuario` (`username`, `fecha_registro`, `nombre`, `contrasena`, `fecha_nacimiento`, `correo`, `avatar`) VALUES
 ('Chit0mx', '2021-06-17', 'Andres Mercado', '1234', '2000-03-17', 'Chit0mx@gmail.com', 2),
-('Jessusu20', '2021-06-14', 'Jesus Martinez', '1234', '0000-00-00', 'jesusumarcor20@gmail.com', 1),
-('Luisf', '2021-06-17', 'Felipe Garc?a', '1234', '2000-03-14', 'luisgarav@gmail.com', 1),
-('TheRaven0', '2021-06-17', 'Kevin Medina', '1234', '2000-10-04', 'kevingenial01@gmail.com', 1),
-('VioletUmi', '2021-06-17', 'Claudia Cortes', '1234', '2000-05-03', 'VioletUmi@gmail.com', 2);
+('Jessusu20', '2021-06-14', 'Jesus Martinez', '1234', '2000-07-20', 'jesusumarcor20@gmail.com', 2),
+('Luisf', '2021-06-17', 'Felipe García', '1234', '2000-03-14', 'luisgarav@gmail.com', 2),
+('TheRaven0', '2021-06-17', 'Kevin Medina', '1234', '2000-10-04', 'kevingenial01@gmail.com', 2),
+('VioletUmi', '2021-06-17', 'Claudia Cortes', '1234', '2000-05-03', 'violetumi@gmail.com', 1);
 
 --
 -- Índices para tablas volcadas
@@ -310,13 +352,13 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de la tabla `comentarios`
 --
 ALTER TABLE `comentarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
 -- AUTO_INCREMENT de la tabla `participacion`
 --
 ALTER TABLE `participacion`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
 
 --
 -- AUTO_INCREMENT de la tabla `pelicula`
@@ -334,7 +376,7 @@ ALTER TABLE `premio`
 -- AUTO_INCREMENT de la tabla `staff`
 --
 ALTER TABLE `staff`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
 
 --
 -- Restricciones para tablas volcadas
