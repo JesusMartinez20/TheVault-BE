c11e32cd3f68e62f0c44a91470a1d0713847c7c1
Kevin Josué Medina Espinosa <38932610+TheRaven0@users.noreply.github.com>
2021-11-23 10:06:21 -0600

    Eliminación de las participaciones y premios relacionados a una pelicula antes de eliminar a esta en eliminarPelicula.php

--- eliminarPelicula.php
+++ eliminarPelicula.php
@@ -7,10 +7,19 @@
         } else{
                     
             mysqli_select_db($conexion,"thevault");
-            
+
+            $eliminaStaff = "DELETE FROM participacion WHERE id_pelicula = $id";
+            $eliminaPremio = "DELETE FROM premio WHERE id_pelicula = $id";
             $elimina = "DELETE FROM pelicula WHERE id = $id";
 
-            mysqli_query($conexion,$elimina) or die (mysqli_error());
+            mysqli_query($conexion,$eliminaStaff) 
+                or die (mysqli_error($conexion));
+
+            mysqli_query($conexion,$eliminaPremio) 
+                or die (mysqli_error($conexion));
+
+            mysqli_query($conexion,$elimina) 
+                or die (mysqli_error($conexion));
 
             mysqli_close($conexion);
         }
