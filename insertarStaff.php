56b04691e9f8232a4f9475c2127400f8762d6f13
Kevin Josué Medina Espinosa <38932610+TheRaven0@users.noreply.github.com>
2021-11-23 10:05:15 -0600

    Corrección en insertarStaff.php para insertar en staff e inmediatamente en participación.

--- insertarStaff.php
+++ insertarStaff.php
@@ -12,11 +12,31 @@
         $queryStaff = "INSERT INTO staff(nombre, nacinalidad, fecha_nacimiento, sexo, imagen)
                         VALUES('$post->nombre', '$post->nacionalidad', '$post->fecha_nacimiento', '$post->sexo', '$post->imagen')";
 
-        $queryParticipacion = "INSERT INTO participacion(rol, id_pelicula, id_staff)
-                                VALUES('$post->rol', '$post->id_pelicula', '$post->id_staff')";
-
         mysqli_query($connection, $queryStaff)
             or die(mysqli_error($connection));
+
+        $getIdQuery = "SELECT id FROM staff ORDER BY id DESC LIMIT 1";
+
+        //Sección para obtener id
+        $resultado = mysqli_query($connection, $getIdQuery)
+            or die(mysqli_error($connection)); 
+
+        $registros = mysqli_num_rows($resultado);
+
+        $datos = array();
+
+        if($registros > 0){
+            while($fila = mysqli_fetch_object($resultado)){
+                $datos[] = $fila;
+            }
+        }
+        //
+
+        $id = $datos[0]->id;
+
+        $queryParticipacion = "INSERT INTO participacion(rol, id_pelicula, id_staff)
+                                VALUES('$post->rol', '$post->id_pelicula', $id)";
+
 
         mysqli_query($connection, $queryParticipacion)
             or die(mysqli_error($connection));
