7efb3f24b7304ee22f91abc5cbd245da73d2fc58
Kevin Josué Medina Espinosa <38932610+TheRaven0@users.noreply.github.com>
2021-11-23 10:07:05 -0600

    Devolver id tras insertar pelicula en insertarPelicula.php

--- insertarPelicula.php
+++ insertarPelicula.php
@@ -9,12 +9,30 @@
     } else {
         mysqli_select_db($connection, "thevault");
 
-        $query = "INSERT INTO staff(nombre, duracion, pais, imagen, fecha_estreno, en_proyeccion)
-                    VALUES('$post->nombre', $post->duracion, '$post->pais', '$post->imagen', $post->fecha_estreno, $post->en_proyeccion)";
+        $query = "INSERT INTO pelicula(nombre, duracion, pais, imagen, fecha_estreno, en_proyeccion)
+                    VALUES('$post->nombre', $post->duracion, '$post->pais', '$post->imagen', '$post->fecha_estreno', '$post->en_proyeccion')";
 
 
         mysqli_query($connection, $query)
             or die(mysqli_error($connection));
+        
+        //
+        $getIdQuery = "SELECT id FROM pelicula ORDER BY id DESC LIMIT 1";
+
+        $resultado = mysqli_query($connection, $getIdQuery)
+            or die(mysqli_error($connection)); 
+
+        $registros = mysqli_num_rows($resultado);
+
+        $datos = array();
+
+        if($registros > 0){
+            while($fila = mysqli_fetch_object($resultado)){
+                $datos[] = $fila;
+            }
+            echo json_encode($datos[0]);
+        }
+        //
 
         mysqli_close($connection);
     }
